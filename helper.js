2438882fb32c1fb4936adc34c3444c8dfe8e26a3
Aymene <belmegaming@gmail.com>
2024-01-18 09:36:31 +0100

    Update helper.js

--- helper.js
+++ helper.js
@@ -1,87 +1,157 @@
-import express from 'express';
-import cors from 'cors';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import http from 'http';
-import helper from './helper.js';
-import jackett from './jackett.js';
+import fetch from 'node-fetch';
 
-const app = express();
-const port = process.env.PORT || 3000;
+const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+function getNum(s) {
+    return s < 10 ? `0${s}` : s.toString();
+}
 
+function selectBiggestFileSeason(files, se) {
+    return files.find(file => se && file.path.includes(se))?.id || null;
+}
 
-const noResults = { streams: [{ url: "#", title: "Aucun résultat trouvé" }] }
+function selectBiggestFileSeasonTorrent(files, se) {
+    const filteredFiles = files.filter(function(file) {
+        return file.name.includes(se);
+    });
 
-
-const server = http.createServer(app);
-const serverTimeout = 120000;
-server.timeout = serverTimeout;
-
-var respond = function (res, data) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-    res.setHeader('Content-Type', 'application/json');
-    res.send(data);
-};
-
-app.use(cors());
-
-app.get('/:debridApi/:service/:jackettUrl/:jackettApi/manifest.json', (req, res) => {
-    const manifest = {
-        id: 'community.aymene69.jackett',
-        version: '1.0.0',
-        catalogs: [],
-        resources: ['stream'],
-        types: ['movie', 'series'],
-        name: 'Jackett',
-        description: 'Stremio Jackett Addon',
-        behaviorHints: {
-            "configurable": true,
-        },
-    };
-    respond(res, manifest);
-});
-
-app.use((err, req, res, next) => {
-    respond(res, noResults);
-});
-
-app.get('/:debridApi/:service/:jackettUrl/:jackettApi/stream/:type/:id', async (req, res) => {
-    try {
-        const type = req.params.type;
-        const id = req.params.id.replace(".json", "").split(':');
-        const debridApi = req.params.debridApi;
-        const service = req.params.service;
-        const jackettUrl = req.params.jackettUrl;
-        const jackettApi = req.params.jackettApi;
-        const mediaName = await helper.getName(id[0], type)
-        if (type == 'movie') {
-            const torrentInfo = await jackett.jackettSearch(debridApi, jackettUrl, jackettApi, service, { name: mediaName, type: type });
-            console.log(torrentInfo[0])
-            respond(res, { "streams": torrentInfo});
-        }
-        if (type == 'series') {
-            const torrentInfo = await jackett.jackettSearch(debridApi, jackettUrl, jackettApi, service, { name: mediaName, type: type, season: helper.getNum(id[1]), episode: helper.getNum(id[2]) });
-            console.log(torrentInfo)
-            respond(res, { "streams": torrentInfo});
-        }
-    } catch (e) {
-        console.log(e)
-        respond(res, noResults);
+    if (filteredFiles.length === 0) {
+        return null;
     }
-});
 
-app.get('/configure', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-})
+    const filesTried = filteredFiles.sort(function(a, b) {
+        return b.length - a.length;
+    });
 
-app.get('/', (req, res) => {
-    res.redirect('/configure');
-})
+        // Trouver l'index du fichier recherché
+    const biggestFileId = files.indexOf(filesTried[0]);
 
-server.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+    return biggestFileId;
+}
+
+function toHomanReadable(bytes) {
+    if (Math.abs(bytes) < 1024) { return bytes + ' B'; }
+  
+    const units = ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  
+    let i = -1;
+    do {
+        bytes /= 1024;
+        ++i;
+    } while (Math.abs(bytes) >= 1024 && i < units.length - 1);
+  
+    return bytes.toFixed(1) + " " + units[i];
+}
+
+function getName(id, type) {
+    if (typeof id === 'string') {
+        const url = 'https://v3-cinemeta.strem.io/meta/' + type + '/' + id + '.json';
+        return fetch(url).then(res => res.json()).then(res => res.meta.name);
+    } else {
+        return id;
+    }
+}
+
+async function addMagnetToRD(magnetLink, debridApi) {
+    const apiUrl = `https://api.real-debrid.com/rest/1.0/torrents/addMagnet`;
+    const headers = {
+    Authorization: `Bearer ${debridApi}`,
+    };
+    const body = new URLSearchParams();
+    body.append('magnet', magnetLink);
+    
+    const response = await fetch(apiUrl, { method: 'POST', headers, body })
+    const responseJson = await response.json()
+    return responseJson.id
+}
+
+async function setMovieFileRD(torrentId, debridApi, seasonEpisode) {
+    let responseJson
+    while (true) {
+        let apiUrl = `https://api.real-debrid.com/rest/1.0/torrents/info/${torrentId}`;
+        let headers = {
+        Authorization: `Bearer ${debridApi}`,
+        };
+    
+        let response = await fetch(apiUrl, { method: 'GET', headers })
+        responseJson = await response.json()
+        let file_status = responseJson["status"]
+        if (file_status != "magnet_conversion") {
+            break
+        }
+        await wait(5000)
+    }
+    let torrentFiles = responseJson["files"]
+    let maxIndex
+    if (seasonEpisode) {
+        maxIndex = selectBiggestFileSeason(torrentFiles, seasonEpisode)
+    }
+    else { 
+        maxIndex = torrentFiles.reduce((maxIndex, file, currentIndex, array) => {
+            const currentBytes = file["bytes"] || 0;
+            const maxBytes = array[maxIndex] ? array[maxIndex]["bytes"] || 0 : 0;
+        
+            return currentBytes > maxBytes ? currentIndex : maxIndex;
+        }, 0);
+    }
+    let torrentFileId
+    if (seasonEpisode)
+        torrentFileId = torrentFiles[maxIndex-1]["id"]
+    else
+        torrentFileId = torrentFiles[maxIndex]["id"]
+    let apiUrl = `https://api.real-debrid.com/rest/1.0/torrents/selectFiles/${torrentId}`;
+    let headers = {
+    Authorization: `Bearer ${debridApi}`,
+    };
+    let body = new URLSearchParams();
+    body.append('files', torrentFileId);
+    let response = await fetch(apiUrl, { method: 'POST', headers, body })
+
+}
+
+async function getMovieRDLink(torrentLink, debridApi, seasonEpisode) {
+    let torrentId = await addMagnetToRD(torrentLink, debridApi)
+    if (seasonEpisode) {
+        await setMovieFileRD(torrentId, debridApi, seasonEpisode)
+    }
+    else {
+        await setMovieFileRD(torrentId, debridApi)
+    }
+    let responseJson
+    while (true) {
+        let apiUrl = `https://api.real-debrid.com/rest/1.0/torrents/info/${torrentId}`;
+        let headers = {
+        Authorization: `Bearer ${debridApi}`,
+        };
+    
+        let response = await fetch(apiUrl, { method: 'GET', headers })
+        responseJson = await response.json()
+        let links = responseJson["links"]
+        if (links.length >= 1) {
+            break
+        }
+        await wait(5000)
+    }
+
+    
+    let downloadLink = responseJson["links"][0]
+
+    let apiUrl = `https://api.real-debrid.com/rest/1.0/unrestrict/link`
+    let headers = {
+    Authorization: `Bearer ${debridApi}`,
+    };
+    let body = new URLSearchParams();
+    body.append('link', downloadLink);
+    let response = await fetch(apiUrl, { method: 'POST', headers, body })
+    responseJson = await response.json()
+    let mediaLink = responseJson["download"]
+    return mediaLink
+}
+export default {
+    getNum,
+    selectBiggestFileSeasonTorrent,
+    selectBiggestFileSeason,
+    toHomanReadable,
+    getName,
+    getMovieRDLink
+};
