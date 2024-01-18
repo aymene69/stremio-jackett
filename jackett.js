51f6ca9d9db2b3f417452f5e3be05c13854ac3a5
Aymene <belmegaming@gmail.com>
2024-01-18 20:51:58 +0100

    Update jackett.js

--- jackett.js
+++ jackett.js
@@ -17,7 +17,6 @@ async function processXML(xml) {
                 return;
             }
 
-            // Accéder aux éléments 'title', 'size', 'link' et 'seeders'
             const items = result.rss.channel.item;
 
             if (!items) {
@@ -74,15 +73,16 @@ async function getTorrentInfo(torrentLink) {
 }
 
 async function jackettSearch(debridApi, jackettHost, jackettApiKey, addonType, searchQuery) {
-    console.log(searchQuery)
     if (searchQuery.type == 'movie') {
         try {
-            const searchUrl = jackettHost + '/api/v2.0/indexers/all/results/torznab/api?apikey=' + jackettApiKey + '&cat=' + "2000" + '&q=' + encodeURIComponent(searchQuery.name)
+            console.log("Searching on Jackett...")
+            const searchUrl = jackettHost + '/api/v2.0/indexers/all/results/torznab/api?apikey=' + jackettApiKey + '&cat=2000&q=' + encodeURIComponent(searchQuery.name)
             let response = await fetch(searchUrl);
             let responseXml = await response.text()
             let items = await processXML(responseXml);
             let results = [];
             if (addonType == 'torrent') {
+                console.log("Will return Torrents.")
                 for (let i = 0; i < 5 ; i++) {
                     let item = items[i];
                     let torrentInfo = await getTorrentInfo(item.link)
@@ -90,15 +90,21 @@ async function jackettSearch(debridApi, jackettHost, jackettApiKey, addonType, s
                     torrentInfo.title = item.title + "\r\n👤" + item.seeders + " 📁" + helper.toHomanReadable(item.size)
                     delete torrentInfo.fileIdx
                     results.push(torrentInfo);
+                    console.log("Added torrent to results: " + item.title)
                 }
                 if (results.length == 0) {
+                    console.log("No results found.")
                     results.push({ name: "Jackett", title: "Aucun résultat trouvé", url: "#" })
                 }
             }
             else {
+                console.log("Will return Debrid links.")
                 for (let i = 0; i < items.length; i++) {
                     let item = items[i];
+                    console.log("Getting torrent info...")
                     let torrentInfo = await getTorrentInfo(item.link)
+                    console.log("Torrent info: " + item.title)
+                    console.log("Getting RD link...")
                     let downloadLink = await helper.getMovieRDLink(torrentInfo.magnetLink, debridApi)
                     let torrentReturn = {
                         name: "Jackett Debrid",
@@ -112,61 +118,76 @@ async function jackettSearch(debridApi, jackettHost, jackettApiKey, addonType, s
                     results.push({ name: "Jackett Debrid", title: "Aucun résultat trouvé", url: "#" })
                 }
             }
+            console.log("Returning results.")
             return results;
         } catch (e) {
             let results = [];
             results.push({ name: "Jackett", title: "Aucun résultat trouvé", url: "#" })
+            console.log("Error: " + e)
             return results;
         }
     }
     if (searchQuery.type == 'series') {
         try {
+            console.log("Searching on Jackett...")
             const searchUrl = jackettHost + '/api/v2.0/indexers/all/results/torznab/api?apikey=' + jackettApiKey + '&cat=5000' + '&q=' + encodeURIComponent(searchQuery.name) + '+S' + searchQuery.season + 'E' + searchQuery.episode
             let response = await fetch(searchUrl);
             let responseXml = await response.text()
             let items = await processXML(responseXml);
             let results = [];
-            console.log(searchUrl)
-            console.log(items[0])
+
             if (addonType == 'torrent') {
+                console.log("Will return Torrents.")
                 for (let i = 0; i < 5 ; i++) {
                     let item = items[i];
                     if (!item) {
                         break
                     }
+                    console.log("Getting torrent info...")
                     let torrentInfo = await getTorrentInfo(item.link)
+                    console.log("Torrent info: " + item.title)
                     torrentInfo.seeders = item.seeders
                     torrentInfo.title = item.title + "\r\n👤" + item.seeders + " 📁" + helper.toHomanReadable(item.size)
                     results.push(torrentInfo);
+                    console.log("Added torrent to results: " + item.title)
                 }
                 if (results.length == 0) {
-                    if (results.length == 0) {
-                        const searchUrl = jackettHost + '/api/v2.0/indexers/all/results/torznab/api?apikey=' + jackettApiKey + '&cat=5000&q=' + encodeURIComponent(searchQuery.name) + '+S' + searchQuery.season
-                        let response = await fetch(searchUrl);
-                        let responseXml = await response.text()
-                        let items = await processXML(responseXml);
-                        let results = [];
-                        for (let i = 0; i < items.length; i++) {
-                            let item = items[i];
-                            let torrentInfo = await getTorrentInfo(item.link)
-                            torrentInfo.seeders = item.seeders
-                            torrentInfo.title = item.title + "\r\n👤" + item.seeders + " 📁" + helper.toHomanReadable(item.size)
-                            torrentInfo.fileIdx = parseInt(helper.selectBiggestFileSeasonTorrent(torrentInfo.files, "S" + searchQuery.season+ "E" + searchQuery.episode), 10)
-                            console.log(torrentInfo.files[torrentInfo.fileIdx])
-                            results.push(torrentInfo);
-                        }
-                        if (results.length == 0) {
-                            results.push({ name: "Jackett", title: "Aucun résultat trouvé", url: "#" })
-                        }
-                        return results;
+                    console.log("No results found with season/episode. Trying without...")
+                    console.log("Searching on Jackett...")
+                    const searchUrl = jackettHost + '/api/v2.0/indexers/all/results/torznab/api?apikey=' + jackettApiKey + '&cat=5000&q=' + encodeURIComponent(searchQuery.name) + '+S' + searchQuery.season
+                    let response = await fetch(searchUrl);
+                    let responseXml = await response.text()
+                    let items = await processXML(responseXml);
+                    let results = [];
+                    for (let i = 0; i < items.length; i++) {
+                        let item = items[i];
+                        console.log("Getting torrent info...")
+                        let torrentInfo = await getTorrentInfo(item.link)
+                        console.log("Torrent info: " + item.title)
+                        torrentInfo.seeders = item.seeders
+                        torrentInfo.title = item.title + "\r\n👤" + item.seeders + " 📁" + helper.toHomanReadable(item.size)
+                        console.log("Determining episode file...")
+                        torrentInfo.fileIdx = parseInt(helper.selectBiggestFileSeasonTorrent(torrentInfo.files, "S" + searchQuery.season+ "E" + searchQuery.episode), 10)
+                        console.log("Episode file determined.")
+                        results.push(torrentInfo);
+                        console.log("Added torrent to results: " + item.title)
                     }
+                    if (results.length == 0) {
+                        console.log("No results found.")
+                        results.push({ name: "Jackett", title: "Aucun résultat trouvé", url: "#" })
+                    }
+                    return results;
                 }
                 return results;
             }
             else {
+                console.log("Will return Debrid links.")
                 for (let i = 0; i < items.length; i++) {
                     let item = items[i];
+                    console.log("Getting torrent info...")
                     let torrentInfo = await getTorrentInfo(item.link)
+                    console.log("Torrent info: " + item.title)
+                    console.log("Getting RD link...")
                     let downloadLink = await helper.getMovieRDLink(torrentInfo.magnetLink, debridApi)
                     let torrentReturn = {
                         name: "Jackett Debrid",
