00f8dea331fb6ca93048b8d79c20a4386da50ed7
Aran Leite <hyoretsu@gmail.com>
2024-01-28 01:00:11 +0000

    Auto-update misc improvements

--- src/helpers/updateApp.js
+++ src/helpers/updateApp.js
@@ -1,36 +1,40 @@
-import fs from 'fs/promises';
-import fetch from 'node-fetch';
-import { exec } from 'child_process';
-import os from 'os';
+import { exec } from "child_process";
+import fetch from "node-fetch";
+import os from "os";
 import { version as localVersion } from "../../package.json";
 
 async function getAppVersionGithub() {
-    const githubJson = await fetch('https://api.github.com/repos/aymene69/stremio-jackett/releases/latest');
-    const githubJsonContent = await githubJson.json();
-    // @ts-ignore
-    const appVersion = githubJsonContent.tag_name;
-    return appVersion.replace('v', '');
+	/** @type {GitHub.Release} */
+	// @ts-ignore
+	const latestRelease = await (
+		await fetch("https://api.github.com/repos/aymene69/stremio-jackett/releases/latest")
+	).json();
+
+	const appVersion = latestRelease.tag_name;
+
+	return appVersion.replace("v", "");
 }
 
 export async function updateApp() {
-    const appVersionLocal = localVersion
-    const appVersionGithub = await getAppVersionGithub();
-    if (appVersionLocal !== appVersionGithub) {
-        console.log('A new update is available!');
-        console.log('Local version:', appVersionLocal, 'Github version:', appVersionGithub);
-        console.log('Updating app...');
-        if (os.platform() === 'win32') {
-            const updateCmd = "git clone https://github.com/aymene69/stremio-jackett temp & xcopy temp\\* .\\ /E /I /Q & rmdir /S /Q temp & npm install";
-            await exec(updateCmd);
-        }
-        else {
-            const updateCmd = "git clone https://github.com/aymene69/stremio-jackett temp && sudo rsync -a temp/ ./ && sudo rm -r temp && npm install";
-            await exec(updateCmd);
-        }
-        console.log('App updated.');
-    }
-    else {
-        console.log('App is up to date.');
-        console.log('Local version:', appVersionLocal, 'Github version:', appVersionGithub)
-    }
+	const latestVersion = await getAppVersionGithub();
+
+	if (localVersion === latestVersion) {
+		return;
+	}
+
+	console.log("A new update is available!");
+	console.log("Local version:", localVersion, "GitHub version:", latestVersion);
+	console.log("Updating app...");
+
+	let updateCmd = "git clone https://github.com/aymene69/stremio-jackett temp";
+	if (os.platform() === "win32") {
+		updateCmd += " & xcopy temp\\* .\\ /E /I /Q & rmdir /S /Q temp & ";
+	} else {
+		updateCmd += " && sudo rsync -a temp/ ./ && sudo rm -r temp && ";
+	}
+	updateCmd += "npm install";
+
+	exec(updateCmd);
+
+	console.log("App updated.");
 }
