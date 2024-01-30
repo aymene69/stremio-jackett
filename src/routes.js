392be46e2f4e917cec3b06ff215d07373b800b72
Aran Leite <hyoretsu@gmail.com>
2024-01-30 23:45:53 +0000

    Synced all versions with package.json

--- src/routes.js
+++ src/routes.js
@@ -22,7 +22,7 @@ routes.get("/:params/manifest.json", (req, res) => {
 	const manifest = {
 		id: "community.aymene69.jackett",
 		icon: "https://i.imgur.com/tVjqEJP.png",
-		version: "1.2.5",
+		version,
 		catalogs: [],
 		resources: ["stream"],
 		types: ["movie", "series"],
