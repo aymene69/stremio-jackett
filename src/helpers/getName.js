a052b8355be66a7caab4e75be665c610e00722e3
Aran Leite <hyoretsu@gmail.com>
2024-01-31 00:29:27 +0000

    Fix getName() bug

--- src/helpers/getName.js
+++ src/helpers/getName.js
@@ -4,7 +4,8 @@ export async function getName(id, type, tmdbApiKey, locale) {
 	if (typeof id !== "string") {
 		return id;
 	}
-	if (tmdbApiKey === undefined) {
+
+	if (!tmdbApiKey) {
 		const res = await fetch(`https://v3-cinemeta.strem.io/meta/${type}/${id}.json`);
 		// @ts-ignore
 		const { meta } = await res.json();
@@ -17,25 +18,27 @@ export async function getName(id, type, tmdbApiKey, locale) {
 			year: releaseInfo,
 		};
 	}
+
 	const res = await fetch(
 		`https://api.themoviedb.org/3/find/${id}?api_key=${tmdbApiKey}&external_source=imdb_id&language=${locale}`,
 	);
+
 	if (type === "movie") {
 		// @ts-ignore
 		const { movie_results } = await res.json();
-		const { title } = movie_results[0];
-		const { release_date } = movie_results[0];
+		const { release_date, title: name } = movie_results[0];
+
 		return {
-			name: title,
+			name,
 			year: release_date.substring(0, 4),
 		};
 	}
+
 	if (type === "series") {
 		// @ts-ignore
 		const { tv_results } = await res.json();
 		const { name } = tv_results[0];
-		return {
-			name: name,
-		};
+
+		return { name };
 	}
 }
