e7dd99d6c85e51b3be9056f007e8f4a4f8b8c8b5
davidemarcoli <davide@marcoli.ch>
2024-02-28 21:06:59 +0100

    refactor search_cache

--- utils/get_cached.py
+++ utils/get_cached.py
@@ -8,12 +8,7 @@ logger = setup_logger(__name__)
 
 def search_cache(query):
     logger.info(query)
-    logger.info("Searching for cached movies on remote server")
-    if query['type'] == "movie":
-        url = CACHER_URL + "getResult/movie/"
-        response = requests.get(url, json=query)
-        return response.json()
-    if query['type'] == "series":
-        url = CACHER_URL + "getResult/series/"
-        response = requests.get(url, json=query)
-        return response.json()
+    logger.info("Searching for cached " + query['type'] + " results")
+    url = CACHER_URL + "getResult/" + query['type'] + "/"
+    response = requests.get(url, json=query)
+    return response.json()
