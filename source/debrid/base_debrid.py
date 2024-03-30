77fea2e95f521c5f0cdbec2cf96e1f23e3462808
itsvncl <75675591+itsvncl@users.noreply.github.com>
2024-03-30 23:17:31 +0100

    Add logs

--- source/debrid/base_debrid.py
+++ source/debrid/base_debrid.py
@@ -35,11 +35,14 @@ class BaseDebrid:
             return None
 
     def wait_for_ready_status(self, check_status_func, timeout=30, interval=5):
+        self.logger.info(f"Waiting for {timeout} seconds to cache.")
         start_time = time.time()
         while time.time() - start_time < timeout:
             if check_status_func():
+                self.logger.info("File is ready!")
                 return True
             time.sleep(interval)
+        self.logger.info(f"Waiting timed out.")
         return False
 
     def donwload_torrent_file(self, download_url):
--- source/debrid/premiumize.py
+++ source/debrid/premiumize.py
@@ -30,8 +30,10 @@ class Premiumize(BaseDebrid):
 
     def get_folder_or_file_details(self, item_id, is_folder=True):
         if is_folder:
+            logger.info(f"Getting folder details with id: {item_id}")
             url = f"{self.base_url}/folder/list?id={item_id}&apikey={self.config['debridKey']}"
         else:
+            logger.info(f"Getting file details with id: {item_id}")
             url = f"{self.base_url}/item/details?id={item_id}&apikey={self.config['debridKey']}"
         return self.get_json_response(url)
     
@@ -46,16 +48,24 @@ class Premiumize(BaseDebrid):
     def get_stream_link(self, query):
         query = json.loads(query)
         magnet = query['magnet']
+        logger.info(f"Received query for magnet: {magnet}")
         info_hash = get_info_hash_from_magnet(magnet)
+        logger.info(f"Info hash extracted: {info_hash}")
         stream_type = query['type']
-        #torrent_download = unquote(query["torrent_download"]) if query["torrent_download"] is not None else None
+        logger.info(f"Stream type: {stream_type}")
         
         transfer_data = self.add_magnet(magnet)
         if not transfer_data or 'id' not in transfer_data:
+            logger.error("Failed to create transfer.")
             return "Error: Failed to create transfer."
         transfer_id = transfer_data['id']
+        logger.info(f"Transfer created with ID: {transfer_id}")
+
         if not self.wait_for_ready_status(lambda: self.get_availability(info_hash)["transcoded"][0] == True):
+            logger.info("Torrent not ready, caching in progress")
             return NO_CACHE_VIDEO_URL
+        
+        logger.info("Torrent is ready.")
 
         # Assuming the transfer is complete, we need to find whether it's a file or a folder
         transfers = self.list_transfers()
@@ -70,18 +80,21 @@ class Premiumize(BaseDebrid):
                 break
 
         if not item_id:
+            logger.error("Transfer completed but no item ID found.")
             return "Error: Transfer completed but no item ID found."
 
         details = self.get_folder_or_file_details(item_id, is_folder)
+        logger.info(f"Got details")
 
         if stream_type == "movie":
+            logger.info("Getting link for movie")
             # For movies, we pick the largest file in the folder or the file itself
             if is_folder:
                 link = max(details.get("content", []), key=lambda x: x["size"])["link"]
             else:
                 link = details.get('link')
-                
         elif stream_type == "series":
+            logger.info("Getting link for series")
             if is_folder:
                 season = query["season"]
                 episode = query["episode"]
@@ -99,12 +112,15 @@ class Premiumize(BaseDebrid):
                     matching_files = strict_matching_files
                     
                 if len(matching_files) == 0:
-                    return "Error: No matching files for {season} {episode} in torrent."
+                    logger.error(f"No matching files for {season} {episode} in torrent.")
+                    return f"Error: No matching files for {season} {episode} in torrent."
                 
                 link = max(matching_files, key=lambda x: x["size"])["link"]
             else:
                 link = details.get('link')
         else:
+            logger.error("Unsupported stream type.")
             return "Error: Unsupported stream type."
 
+        logger.info(f"Link generated: {link}")
         return link
