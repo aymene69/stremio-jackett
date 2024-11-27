c425fd8178261829bb1d7ca4c14825aa4bc2b6c3
aymene69 <aymene69@github.com>
2024-11-27 10:38:16 +0100

    Fixed AllDebrid bug

--- source/debrid/alldebrid.py
+++ source/debrid/alldebrid.py
@@ -61,21 +61,16 @@ class AllDebrid(BaseDebrid):
             season = query['season']
             episode = query['episode']
             logger.info(f"Getting link for series {season}, {episode}")
-            strict_matching_files = []
             matching_files = []
             rank = 0
             if 'e' in data["magnets"]["files"][0].keys():
                 for file in data["magnets"]["files"][0]["e"]:
-                    if season_episode_in_filename(file["n"], season, episode, strict=True):
-                        strict_matching_files.append(file)
-                    elif season_episode_in_filename(file["n"], season, episode, strict=False):
+                    if season_episode_in_filename(file["n"], season, episode):
                         matching_files.append(file)
                     rank += 1
             else:
                 for file in data["magnets"]["files"]:
-                    if season_episode_in_filename(file["n"], season, episode, strict=True):
-                        strict_matching_files.append(file)
-                    elif season_episode_in_filename(file["n"], season, episode, strict=False):
+                    if season_episode_in_filename(file["n"], season, episode):
                         matching_files.append(file)
                     rank += 1
 
