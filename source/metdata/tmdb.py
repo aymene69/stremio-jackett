d433120fab74de0ea9685a9da99a28576a2ca73e
Victor Guerra Veloso <victorgvbh@gmail.com>
2024-11-30 01:46:08 -0500

    When getAllLanguages is set, use indexers' language as languages should be empty

--- source/metdata/tmdb.py
+++ source/metdata/tmdb.py
@@ -7,6 +7,10 @@ from models.series import Series
 from utils.logger import setup_logger
 
 class TMDB(MetadataProvider):
+    def __init__(self, config):
+        super().__init__(config)
+        self._indexers = None
+        
     @property
     def indexers(self):
         return self._indexers
@@ -20,19 +24,22 @@ class TMDB(MetadataProvider):
         full_id = id.split(":")
 
         result = None
-
-        for lang in self.config['languages']:
+        if self.config['getAllLanguages'] and self._indexers and len(self._indexers) > 0:
+          languages = list({indexer.language for indexer in self._indexers}) # Use set to remove duplicated languages
+        else:
+          languages = self.config['languages']
+        for lang in languages:
             url = f"https://api.themoviedb.org/3/find/{full_id[0]}?api_key={self.config['tmdbApi']}&external_source=imdb_id&language={lang}"
             response = requests.get(url)
             data = response.json()
 
-            if lang == self.config['languages'][0]:
+            if lang == languages[0]:
                 if type == "movie":
                     result = Movie(
                         id=id,
                         titles=[self.replace_weird_characters(data["movie_results"][0]["title"])],
                         year=data["movie_results"][0]["release_date"][:4],
-                        languages=self.config['languages']
+                        languages=languages
                     )
                 else:
                     result = Series(
@@ -40,7 +47,7 @@ class TMDB(MetadataProvider):
                         titles=[self.replace_weird_characters(data["tv_results"][0]["name"])],
                         season="S{:02d}".format(int(full_id[1])),
                         episode="E{:02d}".format(int(full_id[2])),
-                        languages=self.config['languages']
+                        languages=languages
                     )
             else:
                 if type == "movie":
