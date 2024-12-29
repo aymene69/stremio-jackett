0098d693264d19af4ed86f9385e2e253da8a7c0a
davidemarcoli <davide@marcoli.ch>
2024-12-29 12:57:03 +0100

    fix: jackett timeout error handling fix: results per quality filter fix: sorting by resolution

--- source/utils/filter/results_per_quality_filter.py
+++ source/utils/filter/results_per_quality_filter.py
@@ -13,17 +13,13 @@ class ResultsPerQualityFilter(BaseFilter):
         resolution_count = {}
         for item in data:
             logger.info(f"Filtering by quality: {item.parsed_data.resolution}")
-            for resolution in item.parsed_data.resolution:
-                if resolution not in resolution_count:
-                    resolution_count[resolution] = 1
-                    filtered_items.append(item)
-                else:
-                    if resolution_count[resolution] < int(self.config['resultsPerQuality']):
-                        resolution_count[resolution] += 1
-                        filtered_items.append(item)
-            else:
+            if item.parsed_data.resolution not in resolution_count:
+                resolution_count[item.parsed_data.resolution] = 1
                 filtered_items.append(item)
-
+            else:
+                if resolution_count[item.parsed_data.resolution] < int(self.config['resultsPerQuality']):
+                    resolution_count[item.parsed_data.resolution] += 1
+                    filtered_items.append(item)
         return filtered_items
 
     def can_filter(self):
